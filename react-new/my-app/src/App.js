75bef4d4eb0297f8722ecad76a94003851282de5
leroid <sorokobochkav@gmail.com>
2023-07-10 19:32:19 +0300

    git add

---
+++ react-new/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HeaderMenu from "./components/HeaderMenu";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello World!</h1>
+      <HeaderMenu></HeaderMenu>
+    </div>
+  );
+}
+
+export default App;
